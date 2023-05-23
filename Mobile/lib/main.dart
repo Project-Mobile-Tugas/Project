2ec569be4a32da87846eb4bf0879e2cf4826a4a3
cahbayu <86003665+cahbayu@users.noreply.github.com>
2023-05-24 01:34:01 +0700

    UAS

--- Mobile/lib/main.dart
+++ Mobile/lib/main.dart
@@ -10,8 +10,8 @@ import 'package:myapp/utils.dart';
 //import 'package:myapp/page-1/kuah.dart';
 //import 'package:myapp/page-1/nasi.dart';
 //import 'package:myapp/page-1/Daftar.dart';
-//import 'package:myapp/page-1/Transaksi.dart';
-import 'package:myapp/page-1/confirm-order.dart';
+import 'package:myapp/page-1/Transaksi.dart';
+// /import 'package:myapp/page-1/confirm-order.dart';
 // import 'package:myapp/page-1/frame-3.dart';
 // import 'package:myapp/page-1/frame-2.dart';
 
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         body: SingleChildScrollView(
-          child: Home(),
+          child: Scene(),
         ),
       ),
     );
